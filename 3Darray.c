d8df0c81fd03df898900d73d9d09b47ab0d43a60
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-18 20:50:48 +0530

    Create 3Darray.c

---
+++ 3Darray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int x[2][3][2] = {{{1, 2}, {3, 4}, {6, 3}}, {{5, 4}, {4, 5}, {9, 1}}};
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 2; k++) {
+        printf("Element at x[%i][%i][%i]: ", i, j, k);
+        printf("%d\n", x[i][j][k]);
+      }
+    }
+  }
+}
