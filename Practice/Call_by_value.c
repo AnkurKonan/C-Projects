e975413432e30afd97f9bfb20465a14f65aae98a
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-04-15 22:08:55 +0530

    Create Call_by_value.c

---
+++ Practice/Call_by_value.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swaping_numbers(int x, int y);
+int main() {
+  int a = 10, b = 20;
+  swaping_numbers(a, b);
+  printf("Inside caller: a = %d & b = %d", a, b);
+  return 0;
+}
+void swaping_numbers(int x, int y) {
+  int t;
+  t = x;
+  x = y;
+  y = t;
+  printf("Inside the function: x = %d y = %d\n", x, y);
+}
