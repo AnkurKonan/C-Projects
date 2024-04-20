997eed540cab9874a0fe285c4d8b0fc73762fa74
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-04-20 22:09:43 +0530

    Update bits_field.c

--- Practice/bits_field.c
+++ Practice/bits_field.c
@@ -15,4 +15,20 @@ int main() {
          code.last);
 }
 
-//
+//Specifiing amount of bit the program needs
+#include <stdio.h>
+
+struct date {
+    int d : 5;
+    int m : 4;
+    int y;
+};
+
+int main()
+{
+    printf("Size of date is %lu bytes\n",
+           sizeof(struct date));
+    struct date dt = { 31, 12, 2014 };
+    printf("Date is %d/%d/%d", dt.d, dt.m, dt.y);
+    return 0;
+}
