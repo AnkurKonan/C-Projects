938c4270bec4506a48cd9997b47b2123b9a01d6b
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-04-20 16:54:39 +0530

    Update File_creating.c

--- Practice/File_creating.c
+++ Practice/File_creating.c
@@ -13,3 +13,16 @@ int main() {
   }
   return 0;
 }
+
+// fseek()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  FILE *file;
+  file = fopen("file.txt", "r");
+  fseek(file, 0, SEEK_END);
+  printf("%ld", ftell(file));
+  return 0;
+}
