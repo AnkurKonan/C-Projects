12ad8a2646e8fe176e14101bdf083be87d2bd0b0
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-05-01 17:42:11 +0530

    Update Enums.c

--- Practice/Enums.c
+++ Practice/Enums.c
@@ -14,3 +14,11 @@ int main() {
   printf("%d, %d, %d", Working, Failed, Freezed);
   return 0;
 }
+
+#include <stdio.h>
+enum day { sunday, monday, tuesday, wednesday, thursday, friday, saturday };
+int main() {
+  enum day d = thursday;
+  printf("The day number stored in d is %d", d);
+  return 0;
+}
